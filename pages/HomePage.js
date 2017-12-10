a16502cec65c6bed0b1217ac28422c62f25a29fb
unknown <ben123tu@gmail.com>
2017-12-10 19:21:11 +0200

    work on home page mail complete

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,12 +1,18 @@
 'use strict';
 
+import AppsPre from '../cmps/AppsPre.js'
+
 export default {
     template: `
         <section>
         <h1>HomePage</h1>
+        <apps-pre></apps-pre>
         </section>
     `,
     created(){
         console.log('shalom')
+    },
+    components:{
+        AppsPre
     }
 }
